34a61104e9b147dd1cc342c6d098c5c06fb21e7e
Kyle Burton <kyle.burton@gmail.com>
2015-10-07 20:23:18 -0400

    swtich from git to https urls when cloning

--- emacs/cider.sh
+++ emacs/cider.sh
@@ -12,7 +12,7 @@ bake_task emacs-install-cider "Install Emacs/Clojure Cider IDE"
 function emacs-install-cider () {
   cider_pushd_to_local_path
   if [ ! -d cider ]; then
-    git clone git@github.com:clojure-emacs/cider.git
+    git clone https://github.com/clojure-emacs/cider.git
   fi
 
   cd cider
@@ -26,7 +26,7 @@ bake_task emacs-install-ac-cider "Install Emacs auto completion for Cider"
 function emacs-install-ac-cider () {
   cider_pushd_to_local_path
   if [ ! -d ac-cider ]; then
-    git clone git@github.com:clojure-emacs/ac-cider.git
+    git clone https://github.com/clojure-emacs/ac-cider.git
   fi
 
   cd ac-cider
